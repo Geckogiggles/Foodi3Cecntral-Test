fbcfcc0d4ca2bd4e5ec0b7c832b6d4a36b803b18
Phoenix-Ceri <phoenixceri@protonmail.com>
2023-04-27 19:59:56 -0400

    here you go eva

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,34 +1,31 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './index.scss';
-import Logo from '../../assets/images/svg/Logo-with-black-double-border.svg';
+import Logo from '../../assets/images/png/Logo-w-frame.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook, faUser, faHome, faHeartCircleCheck, faPeopleRoof } from '@fortawesome/free-solid-svg-icons';
-import '../../assets/fonts';
+import '../../assets/fonts/fonts.css';
 
 const Sidebar = () => (
-    <div className='nav-bar'>
-        {/* <Link className='logo' to='/'>
+    <div className='side-nav-bar'>
+        <Link className='logo' to='/'>
             <img src={Logo} alt='Logo with black frame' />
-        </Link> */}
-        <nav>
-            <NavLink exact='true' activeclassname='active' to='/'>
+        </Link>
+        <nav className='sidenav'>
+            <Link exact='true' activeclassname='active' to='/'>
                 <FontAwesomeIcon icon={faHome} color='#819863' />
-            </NavLink>
-            <NavLink exact='true' activeclassname='active' className='profile-link' to='/profile'>
+            </Link>
+            {/* <Link exact='true' activeclassname='active' className='profile-link' to='/profile'>
                 <FontAwesomeIcon icon={faUser} color='#819863' />
-            </NavLink>
-            <NavLink exact='true' activeclassname='active' className='recipes-link' to='/recipes'>
+            </Link> */}
+            <Link exact='true' activeclassname='active' className='recipes-link' to='/search'>
 
                 <FontAwesomeIcon icon={faBook} color='#819863' />
-            </NavLink>
-            <NavLink exact='true' activeclassname='active' className='myPlans-Cart-link' to='/myMealPlanCart'>
-                <FontAwesomeIcon icon={faHeartCircleCheck} color='#819863' />
-            </NavLink>
-            <NavLink exact='true' activeclassname='active' className='about-us-link' to='/aboutUs'>
+            </Link>
+            <Link exact='true' activeclassname='active' className='about-us-link' to='/about'>
                 <FontAwesomeIcon icon={faPeopleRoof} color='#819863' />
-            </NavLink>
+            </Link>
         </nav>
     </div>
 )
 
-export default Sidebar
+export default Sidebar;
